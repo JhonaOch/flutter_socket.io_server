61f7cc58ba73617a92d6a260ff55eff56effdd13
JhonaOch <34308770+JhonaOch@users.noreply.github.com>
2022-04-28 12:11:47 -0400

    Correcciones

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ server.listen( process.env.PORT, ( err ) => {
 
     if ( err ) throw new Error(err);
 
-    console.log('Servidor corriendo en puerto', process.env.PORT );
+    console.log('Servidor corriendo en puerto:', process.env.PORT );
 
 });
 
