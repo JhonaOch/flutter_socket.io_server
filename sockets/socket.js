6cbe994e4836c7eddb75b73310d1b8d15c5e3e36
JhonaOch <34308770+JhonaOch@users.noreply.github.com>
2022-04-27 21:41:27 -0400

    Metodos de bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,20 @@
 const { io } = require('../index');
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand( new Band( 'Breaking Benjamin' ) );
+bands.addBand( new Band( 'Bon Jovi' ) );
+bands.addBand( new Band( 'Héroes del Silencio' ) );
+bands.addBand( new Band( 'Metallica' ) );
 
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands() );
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -14,6 +25,32 @@ io.on('connection', client => {
 
         io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
 
+    });
+
+    // client.on('emitir-mensaje', (payload ) => {
+    //     console.log('Mensaje', payload);
+    //     //Emite a todos menos al que lo envia
+    //     client.broadcast.emit('nuevo-mensaje', payload );
+    // });
+
+
+    client.on('vote-band', (payload ) => {
+        bands.voteBand( payload.id );
+        io.emit('active-bands', bands.getBands() );
+    });
+
+    client.on('add-band', (payload ) => {
+        console.log('Agregando banda', payload);
+        const newband= new Band(payload.name);
+        bands.addBand( newband );
+        io.emit('active-bands', bands.getBands() );
+    });
+
+    client.on('delete-band', (payload ) => {
+        console.log('Delete banda', payload);
+        const id= payload.id;
+        bands.deleteBand( id );
+        io.emit('active-bands', bands.getBands() );
     });
 
 
